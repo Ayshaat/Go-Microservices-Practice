631012aa8478ac168b7d9269f4b1b6a10aa9b87a
Ayshat <190944378+Ayshaat@users.noreply.github.com>
2025-07-07 22:07:55 +0500

    homework8

--- stocks/cmd/main.go
+++ stocks/cmd/main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"log"
+	"os"
 	"stocks/internal/app"
 )
 
 func main() {
-	if err := app.Run(".env.docker"); err != nil {
+	file := os.Getenv("ENV")
+	switch file {
+	case "production":
+		file = ".env.local"
+	case "testing":
+		file = ".env.docker"
+	default:
+		log.Println("Environment variable ENV is not set.")
+		file = ".env.local"
+	}
+
+	if err := app.Run(file); err != nil {
 		log.Fatalf("stocks app failed: %v", err)
 	}
 }
